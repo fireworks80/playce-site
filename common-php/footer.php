ce0d4cad7b39df04fa62f97dd385d4064ffb417a
hanseungchul <schan@osci.kr>
2019-12-11 18:50:41 +0900

    footer 국문 작업 완료

--- common-php/footer.php
+++ common-php/footer.php
@@ -13,12 +13,11 @@
           </a>
           <ul class="address">
             <li>
-              <span data-i18n="reg.num">사업자등록번호</span> : 114-86-94359
+              <span data-i18n="reg-num">사업자등록번호</span> : 114-86-94359
             </li>
             <li data-i18n="representative">대표자 : 장용훈</li>
             <li data-i18n="addr">
-              서울특별시 강남구 테헤란로 83길 32, 5층  (삼성동,
-              나라키움삼성동A빌딩)
+              서울특별시 강남구 테헤란로 83길 32, 5층  (삼성동,나라키움삼성동A빌딩)
             </li>
             <li>Tel. 02-516-0711</li>
             <li>Fax. 02-516-0722</li>
@@ -39,7 +38,7 @@
               <div class="service service--partner">
                 <h2 class="service__tit" data-i18n="partner">파트너</h2>
                 <ul class="service__list">
-                  <li><a href="./partner.html" data-i18n="partner.program">파트너 프로그램</a></li>
+                  <li><a href="./partner.html" data-i18n="partner-program">파트너 프로그램</a></li>
                 </ul>
               </div>
             </div>
@@ -49,7 +48,7 @@
               <ul class="service__list js-resource-list">
                 <li><a href="./resource.html" data-i18n="">Playce WASup</a></li>
                 <li><a href="./resource.html#playce-roro" dta-i18n="">Playce RoRo</a></li>
-                <li><a href="./resource.html#etc" data-i18n="">추가 리소스</a></li>
+                <li><a href="./resource.html#etc" data-i18n="add-resource">추가 리소스</a></li>
               </ul>
             </div>
           </div>
@@ -58,8 +57,8 @@
             <div class="service">
               <h2 class="service__tit" data-i18n="support">고객지원</h2>
               <ul class="service__list">
-                <li><a href="https://jira.osci.kr/servicedesk/customer/portal/21" target="_blank" data-i18n="buy.prd">제품 문의</a></li>
-                <li><a href="https://jira.osci.kr/servicedesk/customer/portal/21" target="_blank" data-i18n="support.tech">기술 지원</a></li>
+                <li><a href="https://jira.osci.kr/servicedesk/customer/portal/21" target="_blank" data-i18n="product-inquiry">제품 문의</a></li>
+                <li><a href="https://jira.osci.kr/servicedesk/customer/portal/21" target="_blank" data-i18n="tech-support">기술 지원</a></li>
                 <li><a href="https://osci.atlassian.net/wiki/spaces/PWUG/pages/19464217/FAQ" target="_blank" data-i18n="qna">자주 묻는 질문</a></li>
                 <li><a href="./free-start.html" data-i18n="freeStart">무료로 시작</a></li>
               </ul>
@@ -69,10 +68,10 @@
               <h2 class="service__tit">Contact</h2>
               <ul class="service__list">
                 <li>
-                  <a href="mailto:sales@osci.kr">제품 문의 - sales@osci.kr</a>
+                  <a href="mailto:sales@osci.kr"><span data-i18n="product-inquiry">제품 문의</span> - sales@osci.kr</a>
                 </li>
                 <li>
-                  <a href="mailto:lab@osci.kr">기술 문의 - lab@osci.kr</a>
+                  <a href="mailto:lab@osci.kr"><span data-i18n="tech-support">기술 문의</span> - lab@osci.kr</a>
                 </li>
               </ul>
             </div>

fcbcc5861cc24919c45d077015d0bada085f64a2
hanseungchul <schan@osci.kr>
2020-01-06 17:51:01 +0900

    개인정보 처리 방침 영문작업 완료

--- common-php/header.php
+++ common-php/header.php
@@ -13,20 +13,20 @@
   
   // logo click 했을때 
   // test 페이지 주석처리
-  // $root_url = $locale === 'en' ? '/'.$locale.'/test' : '/test';
-  $root_url = $locale === 'en' ? '/'.$locale : '/';
+  $root_url = $locale === 'en' ? '/'.$locale.'/test' : '/test';
+  // $root_url = $locale === 'en' ? '/'.$locale : '/';
 
   // gnb 국문 / 영문 클릭했을때
   $query_string = $_SERVER['QUERY_STRING'] ? '?'.$_SERVER['QUERY_STRING'] : '';
-  // $ko_url = '/test/'.basename($_SERVER['PHP_SELF']).$query_string;
-  // $en_url = '/en/test/'.basename($_SERVER['PHP_SELF']).$query_string;
   $ko_url = '/test/'.basename($_SERVER['PHP_SELF']).$query_string;
   $en_url = '/en/test/'.basename($_SERVER['PHP_SELF']).$query_string;
+  // $ko_url = '/'.basename($_SERVER['PHP_SELF']).$query_string;
+  // $en_url = '/en/'.basename($_SERVER['PHP_SELF']).$query_string;
   
 ?>
 <!DOCTYPE html>
 <html lang="<?php echo $locale; ?>">
-<!-- <html lang="en"> -->
+<!-- <html lang="ko"> -->
   <head>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-150725111-1"></script>

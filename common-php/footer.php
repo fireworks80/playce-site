ef021d90e4c55bba12132c955a91c44d26b8cde6
hanseungchul <schan@osci.kr>
2020-01-07 13:11:43 +0900

    archtecture 버튼 모바일 숨기기

--- common-php/footer.php
+++ common-php/footer.php
@@ -174,8 +174,8 @@
 <script src="./js/smooth-anchor.js"></script>
 <script>
   
-  Playce.translation(Playce.getLocale());
-  // Playce.translation('en');
+  // Playce.translation(Playce.getLocale());
+  Playce.translation('en');
 
   (function() {
     'use strict';
